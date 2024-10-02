38167f72bbb7c26bcb95dcafa42f66206fd48ef2
Arsath8597 <arsath8597@gmail.com>
2024-10-02 22:02:14 +0530

    definr verticalTImeLine

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,10 +5,23 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+
 const experience = () => {
   return (
     <section>
       <Heading children="Experience" />
+      <VerticalTimeline>
+        {experiencesData.map((experience, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement>
+              <h3>{experience.title}</h3>
+              <p>{experience.location}</p>
+              <p>{experience.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
   );
 };
